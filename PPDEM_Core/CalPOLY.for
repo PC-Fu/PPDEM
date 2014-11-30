ed0118e0e0a1b1b0ba1ac69ab40bd4f00a9b179e
david4utd <worry4utd@gmail.com>
2014-11-29 21:47:56 -0800

    preparing for adding link damping

--- PPDEM_Core/CalPOLY.for
+++ PPDEM_Core/CalPOLY.for
@@ -15,7 +15,8 @@
      $  eleOut,FWall,iniFWall,FxWall,FyWall,FMWall,Fx,Fy,FM,iniOri,
      $  elong,flagOutInied,factorSlow,
      $  hThinLayer,flagThinLayer,iDirecCyc, 
-     $  flagSpecialLoad, nLinks, iEleLink, l0Link, kLinkPos, kLinkNeg) 
+     $  flagSpecialLoad, nLinks, iEleLink, l0Link, kLinkPos, kLinkNeg,
+     $  linkDampingRatio) 
 		implicit none
 
 	 !DEC$ ATTRIBUTES DLLEXPORT :: CalPOLY
@@ -38,7 +39,8 @@
      $  eleOut,FWall,iniFWall,FxWall,FyWall,FMWall,Fx,Fy,FM,iniOri,
      $  elong,flagOutInied,factorSlow,
      $  hThinLayer,flagThinLayer,iDirecCyc, 
-     $  flagSpecialLoad, nLinks, iEleLink, l0Link, kLinkPos, kLinkNeg
+     $  flagSpecialLoad, nLinks, iEleLink, l0Link, kLinkPos, kLinkNeg,
+     $  linkDampingRatio
      
      
      
@@ -148,7 +150,7 @@ c     $   strgTensl(1)
      $  eleOut,FWall,iniFWall,FxWall,FyWall,FMWall,Fx,Fy,FM,iniOri,
      $  elong,flagOutInied,factorSlow,
      $  hThinLayer,flagThinLayer,iDirecCyc,flagSpecialLoad, 
-     $  nLinks, iEleLink, l0Link, kLinkPos, kLinkNeg)
+     $  nLinks, iEleLink, l0Link, kLinkPos, kLinkNeg, linkDampingRatio)
 
 
 
--- PPDEM_Core/LinkForce.f90
+++ PPDEM_Core/LinkForce.f90
@@ -1,6 +1,8 @@
-subroutine LinkForce(x1, y1, x2, y2, l0, kPos, kNeg, FLink)
+subroutine LinkForce(x1, y1, x2, y2, l0, kPos, kNeg, FLink, &
+    m1, m2, vx1, vx2, vy1, vy2, dampingRatio)
 implicit none
     double precision x1, y1, x2, y2, l0, kPos, kNeg, FLink(2), l
+    double precision m1, m2, vx1, vx2, vy1, vy2, dampingRatio
 
     l = (x1-x2)**2 + (y1-y2)**2
     l = dsqrt(l)
@@ -15,3 +17,8 @@ implicit none
     end if
 
 end subroutine LinkForce  
+xEle(iEle), yEle(iEle), xEle(jEle), yEle(jEle),
+     $         l0Link(iLink), kLinkPos(iLink), kLinkNeg(iLink),
+     $         FLink,  mInertEle(iEle), mInertEle(jEle), vxEle(iEle), 
+     $         vxEle(jEle), vyEle(iEle), vyEle(jEle),
+     $         linkDampingRatio
