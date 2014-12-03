5da975506d010c2b52b5bb4079c48275c0bc0421
david4utd <worry4utd@gmail.com>
2014-12-03 10:48:52 -0800

    no damping for relative rotation in link

--- PPDEM_Core/LinkForce.f90
+++ PPDEM_Core/LinkForce.f90
@@ -1,13 +1,14 @@
 subroutine LinkForce(x1, y1, x2, y2, l0, kPos, kNeg, FLink, &
     m1, m2, vx1, vx2, vy1, vy2, dampingRatio)
 implicit none
-    double precision x1, y1, x2, y2, l0, kPos, kNeg, FLink(2), l
+    double precision x1, y1, x2, y2, l0, kPos, kNeg, FLink(2), l, dv
     double precision m1, m2, vx1, vx2, vy1, vy2, dampingRatio
 
     l = (x1-x2)**2 + (y1-y2)**2
     l = dsqrt(l)
     FLink(1) = x2 - x1
     FLink(2) = y2 - y1
+    dv = (FLink(1) * (vx2 - vx1) + FLink(2) * (vy2 - vy1)) / l
     FLink = Flink / l
 
     if (l .ge. l0) then
@@ -17,11 +18,11 @@ implicit none
     end if
 
     if (l .ge. l0) then
-        FLink(1) = FLink(1) + 2* dampingRatio * dsqrt(kPos / (1/m1 + 1/m2)) * (vx2 - vx1)
-        FLink(2) = FLink(2) + 2* dampingRatio * dsqrt(kPos / (1/m1 + 1/m2)) * (vy2 - vy1)
+        FLink(1) = FLink(1) + 2* dampingRatio * dsqrt(kPos / (1/m1 + 1/m2)) * dv * (x2 - x1) / l
+        FLink(2) = FLink(2) + 2* dampingRatio * dsqrt(kPos / (1/m1 + 1/m2)) * dv * (y2 - y1) / l
     else
-        FLink(1) = FLink(1) + 2* dampingRatio * dsqrt(kNeg / (1/m1 + 1/m2)) * (vx2 - vx1)
-        FLink(2) = FLink(2) + 2* dampingRatio * dsqrt(kNeg / (1/m1 + 1/m2)) * (vy2 - vy1)
+        FLink(1) = FLink(1) + 2* dampingRatio * dsqrt(kNeg / (1/m1 + 1/m2)) * dv * (x2 - x1) / l
+        FLink(2) = FLink(2) + 2* dampingRatio * dsqrt(kNeg / (1/m1 + 1/m2)) * dv * (y2 - y1) / l
     end if
     
 end subroutine LinkForce  
